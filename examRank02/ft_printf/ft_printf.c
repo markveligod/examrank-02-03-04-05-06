eaf8f6791f8c31afa6e6fcb1c40377d1cb070f0b
Vladislaw Velikockij <ckakuna@mi-p5.msk.21-school.ru>
2020-07-30 08:49:51 +0300

    add ft_printf.c

--- examRank02/ft_printf/ft_printf.c
+++ examRank02/ft_printf/ft_printf.c
@@ -2,290 +2,245 @@
 #include <stdlib.h>
 #include <unistd.h>
 
-typedef struct s_lst
+typedef struct s_list
 {
-    int width;
-    int point;
-    int precision;
-} t_lst;
+	int width;
+	int point;
+	int precis;
+} t_list;
 
-t_lst   ft_init_flags(t_lst list_flags)
+t_list init_struct(t_list flags)
 {
-    list_flags.width = 0;
-    list_flags.point = 0;
-    list_flags.precision = 0;
-    return (list_flags);
+	flags.width = 0;
+	flags.point = 0;
+	flags.precis = 0;
+	return (flags);
 }
 
-int		ft_check_flag(char flag)
+int ft_isdigit(char c)
 {
-	return ((flag >= '0' && flag <= '9') || flag == '.');
+	return (c >= '0' && c <= '9');
 }
 
-t_lst   ft_cut_flags(const char *flag, t_lst list_flags)
+int is_spec(char c)
 {
-    int i;
-
-    i = 0;
-    while (flag[i] && ft_check_flag(flag[i]))
-    {
-        if ((flag[i] >= '0' && flag[i] <= '9') && list_flags.point == 0)
-            list_flags.width = (list_flags.width * 10) + (flag[i] - '0');
-        else if (flag[i] == '.')
-            list_flags.point = 1;
-        else if ((flag[i] >= '0' && flag[i] <= '9') && list_flags.point == 1)
-            list_flags.precision = (list_flags.precision * 10) + (flag[i] - '0');
-        i++;
-    }
-    return (list_flags);
+	return (c == 's' || c == 'd' || c == 'x');
 }
 
-int     ft_strlen(char *str)
+t_list pars_flags(t_list flags, char *str)
 {
-    int i;
+	int i = 0;
 
-    i = 0;
-    if (!str)
-        return (0);
-    while (str[i])
-        i++;
-    return (i);
-}
-
-int     check_size(int n)
-{
-	int		count;
-	long	res;
-
-	res = n;
-	count = 1;
-	if (res < 0)
+	while (str[i] && (is_spec(str[i]) == 0))
 	{
-		count++;
-		res *= (-1);
+		if (ft_isdigit(str[i]) && flags.point == 0)
+			flags.width = (flags.width * 10) + (str[i] - '0');
+		else if (str[i] == '.' && flags.point == 0)
+			flags.point = 1;
+		else if (ft_isdigit(str[i]) && flags.point == 1)
+			flags.precis = (flags.precis * 10) + (str[i] - '0');
+		i++;
 	}
-	while (res >= 10)
+	return (flags);
+}
+
+int check_size(int number)
+{
+	unsigned int num;
+	int count = 0;
+
+	if (number < 0)
 	{
 		count++;
-		res /= 10;
+		num = number * (-1);
+	}
+	else
+		num = number;
+	while (num > 0)
+	{
+		count++;
+		num /= 10;
 	}
 	return (count);
 }
 
-char    *ft_itoa(int n)
+int ft_strlen (char *str)
 {
-	char	*array;
-	long	res;
-	int		i;
+	int i = 0;
 
-	i = 0;
-	if (!(array = (char*)malloc(sizeof(char) * check_size(n) + 1)))
-		return (NULL);
-	if (n < 0)
-		array[0] = '-';
-	res = n;
-	if (res < 0)
-		res *= (-1);
-	array[check_size(n) - i++] = '\0';
-	while (res >= 10)
+	while (str[i])
+		i++;
+	return (i);
+}
+
+char *get_num(int number, int size)
+{
+	unsigned num;
+	char *str;
+
+	str = (char *)malloc(sizeof(char) * (size + 1));
+	str[size] = '\0';
+	if (number < 0)
+		num = number * (-1);
+	else
+		num = number;
+	while (--size > 0)
 	{
-		array[check_size(n) - i++] = (res % 10) + '0';
-		res /= 10;
+		str[size] = (num % 10) + '0';
+		num /= 10;
 	}
-	array[check_size(n) - i++] = (res % 10) + '0';
-	return (array);
-}
-
-int     ft_add_width(int size, char c)
-{
-    int i;
-
-    i = 0;
-    while (i < size)
-    {
-        write(1, &c, 1);
-        i++;
-    }
-    return (i);
-}
-
-int     ft_putstr(char *str)
-{
-    int i;
-
-    i = 0;
-    while (str[i])
-    {
-        write(1, &str[i], 1);
-        i++;
-    }
-    return (i);
-}
-
-int     ft_print_int(t_lst list_flags, va_list argv)
-{
-    int count;
-    int num;
-    char *str;
-    int size;
-
-    count = 0;
-    num = va_arg(argv, int);
-    str = ft_itoa(num);
-    size = ft_strlen(str);
-    if (list_flags.width > size)
-        count += ft_add_width((list_flags.precision > size) ? list_flags.precision - size : list_flags.width - size, (list_flags.precision > size) ? '0' : ' ');
-    else
-        count += (list_flags.precision > size) ? ft_add_width(list_flags.precision - size, '0') : 0;
-    count += ft_putstr(str);
-    free(str);
-    return (count);
-}
-
-char     *ft_strdup(char *str)
-{
-    char *array;
-    int i;
-    int size;
-
-	i = 0;
-    size = ft_strlen(str);
-    if(!(array = (char *)malloc(sizeof(char) * (size + 1))))
-        return (NULL);
-    while (str[i])
-    {
-        array[i] = str[i];
-        i++;
-    }
-    array[i] = '\0';
-    return (array);
-}
-
-int     ft_print_str(t_lst list_flags, va_list argv)
-{
-    char	*str;
-	int		size;
-	int		count;
-    int     i;
-
-    count = 0;
-	if (!(str = va_arg(argv, char *)))
-		str = ft_strdup("(null)");
-	size = ft_strlen(str);
-    if (list_flags.width > size)
-        count += ft_add_width(list_flags.width - ((list_flags.precision > 0 && list_flags.precision < size) ? list_flags.precision : size), ' ');
-    if (list_flags.precision > 0 && list_flags.precision < size)
-    {
-        i = 0;
-        while (i < list_flags.precision)
-        {
-            write(1, &str[i], 1);
-            count++;
-            i++;
-        }
-    }
-    else
-        count += ft_putstr(str);
-    return (count);
-}
-
-int		ft_check_num(unsigned long long number, int size)
-{
-	int	count;
-
-	count = 0;
-	while (number > 0)
-	{
-		number /= size;
-		count++;
-	}
-	return (count);
-}
-
-char	*ft_convert_pointer(char *array_hex, unsigned long long number)
-{
-	int		size_hex;
-	int		count_num;
-	char	*str;
-
-	size_hex = ft_strlen(array_hex);
-	count_num = ft_check_num(number, size_hex);
-	if (!(str = (char *)malloc(sizeof(char) * (count_num + 1))))
-		return (NULL);
-	str[count_num] = '\0';
-	while (--count_num)
-	{
-		str[count_num] = array_hex[number % size_hex];
-		number /= size_hex;
-	}
-	if (count_num == 0)
-		str[count_num] = array_hex[number % size_hex];
+	if (number < 0)
+		str[size] = '-';
+	else
+		str[size] = (num % 10) + '0';
 	return (str);
 }
 
-int     ft_print_xxx(char *array_hex, t_lst list_flags, va_list argv)
+int print_width(int size, char c)
 {
-    char				*str;
-	int					size;
-	int					count;
-	unsigned long long	num;
+	int count = 0;
 
-	num = va_arg(argv, unsigned long long);
-	if (!num)
-		str = ft_strdup("0");
-	else
-		str = ft_convert_pointer(array_hex, num);
+	while (count < size)
+	{
+		write(1, &c, 1);
+		count++;
+	}
+	return (count);
+}
+
+int ft_putstr(char *str, int size)
+{
+ 	    int count = 0;
+ 
+ 	    while (count < size)
+	    {
+	        write(1, &str[count], 1);
+	        count++;
+	    }
+	 return (count);
+}
+
+int print_int(t_list flags, va_list value)
+{
+	int count = 0;
+	int number;
+	char *str;
+	int size;
+
+	number = va_arg(value, int);
+	size = check_size(number);
+	str = get_num(number, size);
 	size = ft_strlen(str);
-	count = 0;
-    if (list_flags.width > 0)
-        count += (list_flags.precision > size) ? ft_add_width(list_flags.precision - size, '0') : ft_add_width(list_flags.width - size, ' ');
-    else
-        count += (list_flags.precision > size) ? ft_add_width(list_flags.precision - size, '0') : 0;
-    count += ft_putstr(str);
-    free(str);
-    return (count);
+	if (number < 0)
+		size--;
+	count += print_width(flags.width - ((flags.precis > size ? ((number < 0) ? (flags.precis + 1) : flags.precis) : ((number < 0) ? (size + 1) : size))), ' ');
+	if (number < 0)
+		count += print_width(1, '-');
+	count += print_width(flags.precis - size, '0');
+	count += ft_putstr(((number < 0) ? &str[1] : &str[0]), size);
+	return (count);
 }
 
-int     ft_print_value(char flag, t_lst list_flags, va_list argv)
+int print_str(t_list flags, va_list value)
 {
-    int count;
+	char *str;
+	int size;
+	int count = 0;
 
-    count = 0;
-    if (flag == 'd')
-        count += ft_print_int(list_flags, argv);
-    else if (flag == 's')
-        count += ft_print_str(list_flags, argv);
-    else if (flag == 'x')
-        count += ft_print_xxx("0123456789abcdef", list_flags, argv);
-    return (count);
+	str = va_arg(value, char *);
+	size = ft_strlen(str);
+	count += print_width(flags.width - ((flags.precis > 0 && flags.precis < size) ? flags.precis : size), ' ');
+	count += ft_putstr(str, ((flags.precis > 0 && flags.precis < size) ? flags.precis : size));
+	return (count);
 }
 
-int     ft_printf(const char *str, ... )
+int check_long(unsigned long long number)
 {
-    int count;
-    int i;
-    va_list argv;
-    t_lst list_flags;
+	int count = 0;
 
-    count = 0;
-    i = 0;
-    va_start(argv, str);
-    while (str[i])
-    {
-        count += (str[i] == '%') ? 0 : 1;
-        if (str[i] == '%')
-        {
-            i++;
-            list_flags = ft_init_flags(list_flags);
-            list_flags = ft_cut_flags(&str[i], list_flags);
-            while (ft_check_flag(str[i]))
-                i++;
-            count += ft_print_value(str[i], list_flags, argv);
-        }
-        else
-            write(1, &str[i], 1);
-        i++;
-    }
-    va_end(argv);
-    return (count);
+	while (number > 0)
+	{
+		count++;
+		number /= 16;
+	}
+	return (count);
+}
+
+char *get_long(unsigned long long number)
+{
+	char *str;
+	int size;
+	char *array_hex = "0123456789abcdef";
+
+	size = check_long(number);
+	str = (char *)malloc(sizeof(char) * (size + 1));
+	str[size] = '\0';
+	while (--size > 0)
+	{
+		str[size] = array_hex[number % 16];
+		number /= 16;
+	}
+	str[size] = array_hex[number % 16];
+	return (str);
+}
+
+int print_xxx(t_list flags, va_list value)
+{
+	int count = 0;
+	int size;
+	char *str;
+	unsigned long long number;
+
+	number = va_arg(value, unsigned long long);
+	str = get_long(number);
+	size = ft_strlen(str);
+	count += print_width(flags.width - ((flags.precis > size) ? flags.precis : size), ' ');
+	count += print_width(flags.precis - size, '0');
+	count += ft_putstr(str, size);
+	return (count);
+}
+
+int 	sort_spec(char c, t_list flags, va_list value)
+{
+	int count = 0;
+
+	if (c == 'd')
+		count += print_int(flags, value);
+	if (c == 's')
+		count += print_str(flags, value);
+	if (c == 'x')
+		count += print_xxx(flags, value);
+	return (count);
+}
+
+
+
+int ft_printf(char *str, ...)
+{
+	int count = 0;
+	va_list value;
+	int i = 0;
+	t_list flags;
+
+	va_start(value, str);
+	while (str[i])
+	{
+		if (str[i] == '%')
+		{
+			i++;
+			flags = init_struct(flags);
+			flags = pars_flags(flags, &str[i]);
+			while (is_spec(str[i]) == 0)
+				i++;
+			count += sort_spec(str[i], flags, value);
+		}
+		else
+		{
+			write(1, &str[i], 1);
+			count++;
+		}
+		i++;
+	}
+	va_end(value);
+	return (count);
 }
