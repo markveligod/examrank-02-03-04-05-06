d2e9108e0f06e208788531dccc33ec5825e72576
student <student@iMac-student.local>
2020-08-09 09:47:42 +0300

    fix

--- examRank03/mini_paint/test.sh
+++ examRank03/mini_paint/test.sh
@@ -1,3 +1,26 @@
-gcc mini.c -o mini_paint -Wall -Wextra -Werror
-./mini_paint ./test/ex_1
-./mini_paint ./test/ex_2
+gcc mini.c -Wall -Wextra -Werror
+echo ""
+echo "TEST - <ex_1>"
+echo "NO original"
+./a.out ./test/ex_1
+echo "Original"
+./original ./test/ex_1
+echo ""
+echo "TEST - <ex_2>"
+echo "NO original"
+./a.out ./test/ex_2
+echo "Original"
+./original ./test/ex_2
+echo ""
+echo "TEST - <NULL>"
+echo "NO original"
+./a.out
+echo "Original"
+./original
+echo ""
+echo "TEST - <Error file>"
+echo "NO original"
+./a.out ./test/ex
+echo "Original"
+./original ./test/ex
+rm a.out
