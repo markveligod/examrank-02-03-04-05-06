3aacbfc770d13fad0f3e0b8b8ba61bff138f4e33
markveligod <markveligod@yandex.ru>
2021-01-30 19:08:44 +0300

    delete trash func

--- examRank06/mini_serv.c
+++ examRank06/mini_serv.c
@@ -8,33 +8,6 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-int extract_message(char **buf, char **msg)
-{
-	char	*newbuf;
-	int	i;
-
-	*msg = 0;
-	if (*buf == 0)
-		return (0);
-	i = 0;
-	while ((*buf)[i])
-	{
-		if ((*buf)[i] == '\n')
-		{
-			newbuf = malloc(sizeof(*newbuf) * (strlen(*buf + i + 1) + 1));
-			if (newbuf == NULL)
-				return (-1);
-			strcpy(newbuf, *buf + i + 1);
-			*msg = *buf;
-			(*msg)[i + 1] = 0;
-			*buf = newbuf;
-			return (1);
-		}
-		i++;
-	}
-	return (0);
-}
-
 int max_fd;
 int g_number;
 
